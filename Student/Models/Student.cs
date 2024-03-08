92b904b2d533724dc0cc58603e5f9ba65b81b6e0
ishakalpi <153492730+ishakalpi@users.noreply.github.com>
2024-03-09 00:10:36 +0530

    changes

--- Student/Models/Student.cs
+++ Student/Models/Student.cs
@@ -1,11 +1,18 @@
-﻿namespace Student.Models
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Student.Models
 {
     public class Student
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Gender { get; set; }
 
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Gender { get; set; }
         public string Class { get; set; }
+        public override string ToString() =>
+        JsonSerializer.Serialize<Student>(this);
     }
 }
